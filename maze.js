1fc24120a65020f7fcb7a1226c54d3516a6009c5
ultimatecompbg <baystancho0@gmail.com>
2019-11-17 13:02:31 +0200

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,58 +1,66 @@
+'use strict';
+ 
+var maze = 
+    [
+		['*', '*', '*', ' ', '*', '*', '*'],
+		['*', 's', '*', ' ', ' ', '*', '*'],
+		['*', ' ', '*', '*', ' ', ' ', ' '],
+		['*', ' ', ' ', '*', ' ', '*', '*'],
+		['*', ' ', ' ', ' ', ' ', '*', '*'],
+		['*', '*', '*', ' ', '*', '*', '*'],
+		['*', ' ', ' ', ' ', '*', '*', '*'],
+		['*', ' ', '*', '*', '*', '*', '*'],
+		['*', ' ', '*', '*', '*', '*', '*'],
+    ];
+ 
+var start = {row: 1, column: 1, path: []};
+var queue = [];
+var exitfound = false;
+ 
+bfs(maze);
+ 
+function bfs(maze) {
+    queue.push(start);
+    while(queue.length != 0) {
+        let position = queue.shift();
+ 
+        tryPosition({row:position.row+1, column:position.column, path:position.path});
+        tryPosition({row:position.row-1, column:position.column, path:position.path});
+        tryPosition({row:position.row, column:position.column+1, path:position.path});
+        tryPosition({row:position.row, column:position.column-1, path:position.path});
+    }
+    if(!exitfound) {
+        console.log("No exit found.");
+    }
+}
+ 
+function tryPosition(position) {
+    if(!isInsideMaze(position)) {
+        exitfound = true;
+        print_path(position);
+        return;
+    }
+    if(maze[position.row][position.column] === ' ') {
+        let newPath = position.path.slice();
+        newPath.push({row:position.row, column:position.column});
+        queue.push({row:position.row, column:position.column, path:newPath});
+        maze[position.row][position.column] = 'x';
+    }
+}
+ 
+function isInsideMaze(position) {
+    if(position.row >= 0 && position.row < maze.length && position.column  >= 0 && position.column < maze[0].length) {
+       return true;
+	}
+}
+ 
+function print_path(position) {
+    let path = position.path;
+    path.forEach(step => {
+        console.log(step);
+    });
+    console.log("Exit found!");
+}
+ 
 
-bfs = function () {
-	var maze = [
-		[' ', ' ', ' ', ' '], 
-        [' ', ' ', '*', ' '],
-        ['*', ' ', '*', ' '],
-        [' ', ' ', ' ', ' ']
-		];
-
-    let isValid = function (x, y) {
-        if (x < 0 || y < 0 || x >= bfs.S || y >= bfs.S){
-		return false; }
-		else {
-        return true;
-		 }
-    };
-    countPaths = function (maze, x, y, visited, count) {
-        if (x === bfs.S - 1 && y === bfs.S - 1) {
-            count++;
-            return count;
-        }
-        visited[x][y] = true;
-        if (isValid(x, y) && maze[x][y] === ' ') {
-            if (x + 1 < bfs.S && !visited[x + 1][y])
-                count = countPaths(maze, x + 1, y, visited, count);
-            if (x - 1 >= 0 && !visited[x - 1][y])
-                count = countPaths(maze, x - 1, y, visited, count);
-            if (y + 1 < bfs.S && !visited[x][y + 1])
-                count = countPaths(maze, x, y + 1, visited, count);
-            if (y - 1 >= 0 && !visited[x][y - 1])
-                count = countPaths(maze, x, y - 1, visited, count);
-        }
-        visited[x][y] = false;
-        return count;
-    };
-    checkPaths = function () {
-        
-        var count = 0;
-        var visited = (function (dims) {
-			var allocate = function (dims) { if (dims.length == 0) {
-            return false;
-        }
-        else {
-            var array = [];
-            for (var i = 0; i < dims[0]; i++) {
-                array.push(allocate(dims.slice(1)));
-            }
-            return array;
-        } }; return allocate(dims); })([bfs.S, bfs.S]);
-        count = countPaths(maze, 0, 0, visited, count);
-        console.info("There are " + count + " paths");
-    };
-	
-    return countPaths;
-}();
-bfs.S = 4;
-
-checkPaths();
+    
