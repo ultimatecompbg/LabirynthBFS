448bbe102f6feb0a22a64c803eceabd9063f9361
ultimatecompbg <baystancho0@gmail.com>
2019-12-08 15:03:38 +0200

    ghjfghjfhj

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let appendElement = (canvas, node, maze) => {
   blockRef.setAttribute("fill", colors[maze[node.y][node.x]]);
   blockRef.setAttribute("stroke", "white");
   var svgElem = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
-    useElem = document.createElementNS("http://www.w3.org/2000/svg", "use");
+  var useElem = document.createElementNS("http://www.w3.org/2000/svg", "use");
   useElem.setAttributeNS(
     "http://www.w3.org/1999/xlink",
     "xlink:href",
